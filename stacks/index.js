4ef8af01dd0d15b8dc21054debf2b1f47cc4ecb5
Leonard Husske <leonard.husske@di-unternehmer.com>
2022-09-12 15:52:58 +0200

    Adding the API

--- stacks/index.js
+++ stacks/index.js
@@ -1,12 +1,13 @@
-import { StorageStack } from "./StorageStack";
+import {StorageStack} from './StorageStack';
+import {ApiStack} from './ApiStack';
 
 export default function main(app) {
-  app.setDefaultFunctionProps({
-    runtime: "nodejs16.x",
-    srcPath: "services",
-    bundle: {
-      format: "esm",
-    },
-  });
-  app.stack(StorageStack);
+	app.setDefaultFunctionProps({
+		runtime: 'nodejs16.x',
+		srcPath: 'services',
+		bundle: {
+			format: 'esm',
+		},
+	});
+	app.stack(StorageStack).stack(ApiStack);
 }
